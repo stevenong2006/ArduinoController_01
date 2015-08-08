13c1c54ef339d4fa91b242f53c525f6369e4b8d5
stevenong2006 <stevenong_2006@yahoo.com>
2015-08-08 10:21:04 -0500

    Initial check in

---
+++ ArduinoClientGUIPart01/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ArduinoControllerPart01
+{
+    partial class ArduinoController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogDisplay = new System.Windows.Forms.RichTextBox();
+            this.BinaryControl = new System.Windows.Forms.CheckBox();
+            this.QueryButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.AnalogControl = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.AnalogControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LogDisplay
+            // 
+            this.LogDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.LogDisplay.Location = new System.Drawing.Point(13, 13);
+            this.LogDisplay.Name = "LogDisplay";
+            this.LogDisplay.Size = new System.Drawing.Size(609, 318);
+            this.LogDisplay.TabIndex = 0;
+            this.LogDisplay.Text = "";
+            // 
+            // BinaryControl
+            // 
+            this.BinaryControl.Appearance = System.Windows.Forms.Appearance.Button;
+            this.BinaryControl.Location = new System.Drawing.Point(13, 338);
+            this.BinaryControl.Name = "BinaryControl";
+            this.BinaryControl.Size = new System.Drawing.Size(112, 32);
+            this.BinaryControl.TabIndex = 1;
+            this.BinaryControl.Text = "Off";
+            this.BinaryControl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BinaryControl.UseVisualStyleBackColor = true;
+            this.BinaryControl.CheckedChanged += new System.EventHandler(this.BinaryControl_CheckedChanged);
+            // 
+            // QueryButton
+            // 
+            this.QueryButton.Location = new System.Drawing.Point(135, 338);
+            this.QueryButton.Name = "QueryButton";
+            this.QueryButton.Size = new System.Drawing.Size(113, 31);
+            this.QueryButton.TabIndex = 2;
+            this.QueryButton.Text = "Query";
+            this.QueryButton.UseVisualStyleBackColor = true;
+            this.QueryButton.Click += new System.EventHandler(this.QueryButton_Click);
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(553, 337);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(69, 31);
+            this.QuitButton.TabIndex = 3;
+            this.QuitButton.Text = "Quit";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // AnalogControl
+            // 
+            this.AnalogControl.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.AnalogControl.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.AnalogControl.Location = new System.Drawing.Point(13, 377);
+            this.AnalogControl.Maximum = 5;
+            this.AnalogControl.Name = "AnalogControl";
+            this.AnalogControl.Size = new System.Drawing.Size(235, 45);
+            this.AnalogControl.TabIndex = 4;
+            this.AnalogControl.Scroll += new System.EventHandler(this.AnalogControl_Scroll);
+            // 
+            // ArduinoController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 452);
+            this.Controls.Add(this.AnalogControl);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.QueryButton);
+            this.Controls.Add(this.BinaryControl);
+            this.Controls.Add(this.LogDisplay);
+            this.Name = "ArduinoController";
+            this.Text = "Arduino Controller";
+            ((System.ComponentModel.ISupportInitialize)(this.AnalogControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox LogDisplay;
+        private System.Windows.Forms.CheckBox BinaryControl;
+        private System.Windows.Forms.Button QueryButton;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.TrackBar AnalogControl;
+    }
+}
+
---
+++ ArduinoClientGUIPart01/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace ArduinoControllerPart01
+{
+    public partial class ArduinoController : Form
+    {
+        SerialPort serialPort;
+        enum Command { SET_BINARY = 0, SET_ANALOG, QUERY };
+        enum BinarySet { OFF = 0, ON };
+
+        char[] m_Command = new char[2];
+
+        /// <summary>
+        /// The constructor does the following:
+        /// - Detect the Arduino's com port from the device manager
+        /// - Initiate the serial connection to the Arduino board
+        /// 
+        /// </summary>
+        public ArduinoController()
+        {
+            InitializeComponent();
+
+            // Detecting Arduino's com port from Windows device manager
+            string comPort = AutodetectArduinoPort(); 
+            string msg;
+
+            if (!string.IsNullOrEmpty(comPort))
+            {
+                msg = string.Format("Arduino comPort=[{0}]\n", comPort);
+                LogDisplay.AppendText(msg);
+
+                try
+                {
+                    serialPort = new SerialPort(comPort, 9600, Parity.None, 8, StopBits.One);
+                    serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceiveHandler);
+                    serialPort.Open();
+
+                    if (serialPort.IsOpen)
+                    {
+                        LogDisplay.AppendText("Successfully connected to Arduino\n");
+                    }
+                    else
+                    {
+                        LogDisplay.AppendText("Error: Connection to Arduino failed\n");
+                    }
+
+                }
+                catch (IOException e)
+                {
+                    LogDisplay.AppendText("IOException: " + e.ToString());
+
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    LogDisplay.AppendText("ArgumentOutOfRangeException: " + e.ToString());
+
+                }
+                catch (ArgumentException e)
+                {
+                    LogDisplay.AppendText("ArgumentOutOfRangeException: " + e.ToString());
+
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    LogDisplay.AppendText("UnauthorizedAccessException: " + e.ToString());
+
+                }
+                catch (InvalidOperationException e)
+                {
+                    LogDisplay.AppendText("InvalidOperationException: " + e.ToString());
+
+                }
+            }
+            else // Will not go further is no Arduino board can be found
+            {
+                msg = string.Format("The Arduino cannot be found. Please plug in the board and re-start the client GUI\n");
+                LogDisplay.AppendText(msg);
+                BinaryControl.Enabled = false;
+                QueryButton.Enabled = false;
+                AnalogControl.Enabled = false;
+            }
+
+        }
+
+        /// <summary>
+        /// Autodetect the arduino com port from device manager.
+        /// </summary>
+        /// <returns>The com port if found</returns>
+        private string AutodetectArduinoPort()
+        {
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+
+            try
+            {
+                foreach (ManagementObject item in searcher.Get())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        return deviceId;
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+                LogDisplay.AppendText(e.ToString());
+            }
+            return null;
+        }
+        private void DataReceiveHandler(object sender, SerialDataReceivedEventArgs arg)
+        {
+            SerialPort sp = (SerialPort)sender;
+            try
+            {
+                string incomingMsg = sp.ReadLine();
+                incomingMsg.Trim();
+                LogDisplay.AppendText(incomingMsg);
+            }
+            catch (System.InvalidOperationException e)
+            {
+                string errorMsg = "IOException source: ";
+                errorMsg += e.Source;
+                LogDisplay.AppendText(errorMsg);
+            }
+        }
+
+        private void SendToArduino(string msg)
+        {
+            try
+            {
+                serialPort.Write(msg);
+            }
+            catch (System.InvalidOperationException e)
+            {
+                string errorMsg = "IOException source: ";
+                errorMsg += e.Source;
+                LogDisplay.AppendText(errorMsg);
+            }
+        }
+
+        private void BinaryControl_CheckedChanged(object sender, EventArgs e)
+        {
+            m_Command[0] = (char)Command.SET_BINARY;
+            if (BinaryControl.Checked)
+            {
+                BinaryControl.Text = "On";
+                m_Command[1] = (char)BinarySet.ON;
+            }
+            else
+            {
+                BinaryControl.Text = "Off";
+                m_Command[1] = (char)BinarySet.OFF;
+            }
+
+            SendToArduino(new string(m_Command));
+
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void AnalogControl_Scroll(object sender, EventArgs e)
+        {
+
+            m_Command[0] = (char)Command.SET_ANALOG;
+            m_Command[1] = (char)AnalogControl.Value;
+
+            SendToArduino(new string(m_Command));
+        }
+
+        private void QueryButton_Click(object sender, EventArgs e)
+        {
+            m_Command[0] = (char)Command.QUERY;
+            m_Command[1] = (char)2;
+
+            SendToArduino(new string(m_Command));
+
+        }
+    }
+}
